e04856388b42bcdeb2f92c4b37daf26afa99b2d1
Klajdi <klajdi.gashi01@gmail.com>
2024-07-21 22:27:20 +0200

    SMF-10:NavBar View

--- frontend/src/components/auth/Main/Navbar.js
+++ frontend/src/components/auth/Main/Navbar.js
@@ -1,10 +1,24 @@
 import React from 'react';
-/* import './Navbar.css'; */
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch, faBell, faUserCircle, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { SlGraduation } from "react-icons/sl";
+import './Navbar.css';
 
 function Navbar() {
   return (
     <div className="navbar">
-       {/* Content Should go here */}
+      <div className="logo-icon-container">
+        <SlGraduation className="logo-icon" />
+        <p className="logo-text">Social Media App</p>
+      </div>
+      <div className="search-container">
+        <input type="text" placeholder="Search for friends, groups, pages" className="search-input" />
+        <FontAwesomeIcon icon={faSearch} className="search-icon" />
+      </div>
+      <div className="icons-container">
+        <FontAwesomeIcon icon={faBell} className="navbar-icon" />
+        <FontAwesomeIcon icon={faUserCircle} className="navbar-icon" />
+      </div>
     </div>
   );
 }
