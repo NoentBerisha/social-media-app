7a0cee1baf197bc4ad454635759fe68067f60cb4
BerishaNoent <noentberisha6@gmail.com>
2024-07-22 00:11:51 +0200

    Add RightSideBar
    I have added the right side bar. To be more precise the active friends list and latest activity part.

---
+++ frontend/src/components/auth/Main/profile-icon/ProfileIconComponent.js
@@ -0,0 +1,40 @@
+import React, {useState}from 'react';
+import Avatar from '@mui/material/Avatar';
+import Badge from '@mui/material/Badge';
+import './ProfileIcon.css';
+import { Link } from 'react-router-dom';
+
+function ProfileIconComponent({ name, photoUrl, isOnline, dropdownOptions, text }) {
+  const [isDropdownVisible, setIsDropdownVisible] = useState(false);
+
+  return (
+    <div 
+      className="profile-icon-component" 
+      onMouseEnter={() => setIsDropdownVisible(true)} 
+      onMouseLeave={() => setIsDropdownVisible(false)}
+    >
+      <Badge
+        overlap="circular"
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+        variant="dot"
+        className={`avatar-badge ${isOnline ? '' : 'offline'}`}
+      >
+        <Avatar alt={name} src={photoUrl} />
+      </Badge>
+      <p className="name" dangerouslySetInnerHTML={{ __html: text }}></p>
+      {isDropdownVisible && (
+        <div className="dropdown-menu">
+          {dropdownOptions.map((option, index) => (
+            <div key={index} className="dropdown-item" onClick={() => setIsDropdownVisible(false)}>
+            <Link key={index} className="dropdown-item" to={option.url} >
+              {option.name}
+            </Link>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default ProfileIconComponent;
