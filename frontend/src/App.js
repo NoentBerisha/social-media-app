5bfa63e1fb743baf53c23eeec98cae43ba39714e
BerishaNoent <noentberisha6@gmail.com>
2024-07-22 19:03:08 +0200

    Add Light mode dark mode feature
    I have added light mode dark mode feature. The only thing that has to be done is to add all the colors to the app.css in both light theme and dark theme. And then use the variables instead of hardcoding the colors.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,39 @@
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/auth/Login';
 import SignUp from './components/auth/SignUp';
 import ForgotPassword from './components/auth/ForgotPassword';
 import Main from './components/auth/Main/Main';
+import './App.css';
+import { FaSun, FaMoon } from 'react-icons/fa';
+
 function App() {
+  const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light');
+
+  useEffect(() => {
+    document.body.setAttribute('data-theme', theme);
+    localStorage.setItem('theme', theme);
+  }, [theme]);
+
+  function toggleTheme() {
+    const newTheme = theme === 'dark' ? 'light' : 'dark';
+    setTheme(newTheme);
+  }
+
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/SignUp" element={<SignUp />} />
-        <Route path="/ForgotPassword" element={<ForgotPassword />} />
-        <Route path="/Main" element={<Main />} />
-      </Routes>
-    </Router>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/SignUp" element={<SignUp />} />
+          <Route path="/ForgotPassword" element={<ForgotPassword />} />
+          <Route path="/Main" element={<Main />} />
+        </Routes>
+      </Router>
+      <button onClick={toggleTheme} className="theme-toggle-btn">
+        {theme === 'light' ? <FaMoon className="icon" /> : <FaSun className="icon" />}
+      </button>
+    </>
   );
 }
 
