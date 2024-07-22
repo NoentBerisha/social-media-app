fa5ddac4bbf3f652ff2beffe64f04bde7d39aed4
BerishaNoent <noentberisha6@gmail.com>
2024-07-22 20:55:36 +0200

    Update light mode icon to have a fixed position

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./components/auth/Login";
 import SignUp from "./components/auth/SignUp";
@@ -9,7 +9,6 @@ import { FaSun, FaMoon } from "react-icons/fa";
 
 function App() {
   const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
-  const buttonRef = useRef(null);
 
   useEffect(() => {
     document.body.setAttribute("data-theme", theme);
@@ -20,41 +19,6 @@ function App() {
     const newTheme = theme === "dark" ? "light" : "dark";
     setTheme(newTheme);
   }
-
-  useEffect(() => {
-    const button = buttonRef.current;
-    let offsetX = 0;
-    let offsetY = 0;
-
-
-  button.style.position = 'absolute';
-  button.style.top = '10px';
-  button.style.left = '10px';
-
-    const mouseDownHandler = function (e) {
-      offsetX = e.clientX - button.getBoundingClientRect().left;
-      offsetY = e.clientY - button.getBoundingClientRect().top;
-
-      document.addEventListener("mousemove", mouseMoveHandler);
-      document.addEventListener("mouseup", mouseUpHandler);
-    };
-    const mouseMoveHandler = function (e) {
-      button.style.left = `${e.clientX - offsetX}px`;
-      button.style.top = `${e.clientY - offsetY}px`;
-    };
-
-    const mouseUpHandler = function () {
-      document.removeEventListener("mousemove", mouseMoveHandler);
-      document.removeEventListener("mouseup", mouseUpHandler);
-    };
-
-    button.addEventListener("mousedown", mouseDownHandler);
-
-    return () => {
-      button.removeEventListener("mousedown", mouseDownHandler);
-    };
-  }, []);
-
   return (
     <>
      
@@ -67,12 +31,8 @@ function App() {
         </Routes>
       </Router>
       <button
-        ref={buttonRef}
-        onDoubleClick={toggleTheme}
+        onClick={toggleTheme}
         className="theme-toggle-btn"
-        title="
-       Click and drag to move,
-      Double click to switch between modes"
       >
         {theme === "light" ? (
           <FaMoon className="icon" />
