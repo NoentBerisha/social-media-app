19fedc18e0e2f42861620c4b6ee19b2f73807c10
Art Grantolli <artgrantolli19@gmail.com>
2024-07-23 00:05:24 +0200

    Add SideBar View
    Basically done but my group bar need to be finished

--- frontend/src/components/auth/Main/Sidebar.js
+++ frontend/src/components/auth/Main/Sidebar.js
@@ -1,12 +1,54 @@
-import React from 'react';
-/* import './Sidebar.css'; */
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faUserFriends, faUsers, faShoppingCart, faBookmark, faFileAlt, faStar, faEllipsisH } from '@fortawesome/free-solid-svg-icons';
+import './Sidebar.css'; // Assuming you have a CSS file for styling
 
-function Sidebar() {
+const mainMenuItems = [
+  { name: 'Home', icon: faHome },
+  { name: 'Friends', icon: faUserFriends },
+  { name: 'Groups', icon: faUsers },
+  { name: 'Marketplace', icon: faShoppingCart },
+  { name: 'Saved', icon: faBookmark },
+];
+
+const moreMenuItems = [
+  { name: 'Pages', icon: faFileAlt },
+  { name: 'Favourites', icon: faStar },
+];
+
+const Sidebar = () => {
+  const [showMore, setShowMore] = useState(false);
+
+  const handleToggle = () => {
+    setShowMore(!showMore);
+  };
+
   return (
     <div className="sidebar">
-      {/* Content Should go here */}
+      <ul>
+        {mainMenuItems.map((item, index) => (
+          <li key={index} className="sidebar-item">
+            <FontAwesomeIcon icon={item.icon} className="sidebar-icon" />
+            <span className="sidebar-text">{item.name}</span>
+          </li>
+        ))}
+        <li className="sidebar-item" onClick={handleToggle}>
+          <FontAwesomeIcon icon={faEllipsisH} className="sidebar-icon" />
+          <span className="sidebar-text">See More</span>
+        </li>
+        {showMore && (
+          <div className="more-menu">
+            {moreMenuItems.map((item, index) => (
+              <li key={index} className="sidebar-item">
+                <FontAwesomeIcon icon={item.icon} className="sidebar-icon" />
+                <span className="sidebar-text">{item.name}</span>
+              </li>
+            ))}
+          </div>
+        )}
+      </ul>
     </div>
   );
-}
+};
 
 export default Sidebar;
