11756a71034b79ef41dec60abc64ddf1855414cd
Klajdi <klajdi.gashi01@gmail.com>
2024-08-07 22:35:11 +0200

    SMF-15:SideBarView

--- frontend/src/components/Profile/Sidebar.js
+++ frontend/src/components/Profile/Sidebar.js
@@ -1,32 +1,29 @@
+// Sidebar.js
 import React, { useState } from 'react';
+import './Sidebar.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome, faUserFriends, faUsers, faShoppingCart, faBookmark, faFileAlt, faStar, faEllipsisH } from '@fortawesome/free-solid-svg-icons';
-import './Sidebar.css'; // Assuming you have a CSS file for styling
-import ProfileIconComponent from '../Main/profile-icon/ProfileIconComponent';
-const mainMenuItems = [
-  { name: 'Home', icon: faHome },
-  { name: 'Friends', icon: faUserFriends },
-  { name: 'Groups', icon: faUsers },
-  { name: 'Marketplace', icon: faShoppingCart },
-  { name: 'Saved', icon: faBookmark },
-  { name: 'Pages', icon: faFileAlt },
-  { name: 'Favourites', icon: faStar },
-  { name: 'Pages', icon: faFileAlt },
-  { name: 'Favourites', icon: faStar },
+import { faDribbble, faBehance, faUpwork, faLinkedin, faFacebook, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faChevronDown, faChevronUp } from '@fortawesome/free-solid-svg-icons';
 
+const skills = ["Brand Design", "UI Design", "Figma", "Adobe After Effects", "Sketch", "Pen & Paper"];
+const portfolioLinks = [
+  { name: 'Dribbble', icon: faDribbble },
+  { name: 'Behance', icon: faBehance },
+  { name: 'Upwork', icon: faUpwork },
+  { name: 'LinkedIn', icon: faLinkedin },
 ];
-/*
-const moreMenuItems = [
-  { name: 'Pages', icon: faFileAlt },
-  { name: 'Favourites', icon: faStar },
+const verifications = [
+  { name: 'Facebook', icon: faFacebook },
+  { name: 'LinkedIn', icon: faLinkedin },
+  { name: 'Twitter', icon: faTwitter },
 ];
-*/
-const groups = [
-  { name: '12/5', url: 'https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg?cs=srgb&dl=pexels-justin-shaifer-501272-1222271.jpg&fm=jpg' },
-  { name: 'Xixellonjat', url: 'https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg?cs=srgb&dl=pexels-justin-shaifer-501272-1222271.jpg&fm=jpg' },
-  { name: 'Test', url: 'https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg?cs=srgb&dl=pexels-justin-shaifer-501272-1222271.jpg&fm=jpg' },
-  { name: 'TEST1', url: 'https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg?cs=srgb&dl=pexels-justin-shaifer-501272-1222271.jpg&fm=jpg' },
-];
+
+const SidebarItem = ({ name, icon }) => (
+  <div className="sidebar-item">
+    {icon && <FontAwesomeIcon icon={icon} className="sidebar-icon" />}
+    <span className="sidebar-text">{name}</span>
+  </div>
+);
 
 const Sidebar = () => {
   const [showMore, setShowMore] = useState(false);
@@ -37,30 +34,36 @@ const Sidebar = () => {
 
   return (
     <div className="sidebar">
-      <ul>
-        {mainMenuItems.map((item, index) => (
-          <li key={index} className="sidebar-item">
-            <FontAwesomeIcon icon={item.icon} className="sidebar-icon" />
-            <span className="sidebar-text">{item.name}</span>
-          </li>
+      <div className="sidebar-section skills">
+        <h3>Skills</h3>
+        {skills.map((skill, index) => (
+          <div key={index} className="skill-item">{skill}</div>
         ))}
-        <li className="sidebar-item" onClick={handleToggle}>
-          <FontAwesomeIcon icon={faEllipsisH} className="sidebar-icon" />
-          <span className="sidebar-text">See More</span>
-        </li>
-        {showMore && (
-          <div className="more-menu">
-            <br></br>
-            <h3>My Groups</h3>
-            {groups.map((item, index) => (
-              <li key={index} className="sidebar-item">
-              <ProfileIconComponent name = {item.name} photoUrl = {item.url} text = {item.name} showBadge={false}/>
-            
-              </li>
-            ))}
-          </div>
-        )}
-      </ul>
+      </div>
+
+      <div className="sidebar-section portfolio-links">
+        <h3>Portfolio Links</h3>
+        {portfolioLinks.map((link, index) => (
+          <SidebarItem key={index} name={link.name} icon={link.icon} />
+        ))}
+      </div>
+
+      <div className="sidebar-section verifications">
+        <h3>Verifications</h3>
+        {verifications.map((verification, index) => (
+          <SidebarItem key={index} name={verification.name} icon={verification.icon} />
+        ))}
+      </div>
+
+      <div className="sidebar-section more-menu-toggle" onClick={handleToggle}>
+        <SidebarItem name="See More" icon={showMore ? faChevronUp : faChevronDown} />
+      </div>
+      {showMore && (
+        <div className="sidebar-section more-menu">
+          <h3>Additional Info</h3>
+          {/* Add more dynamic content here */}
+        </div>
+      )}
     </div>
   );
 };
